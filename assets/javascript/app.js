f24429bb0f157418955be37c0b9cfeede24074e9
Jason Ostergren <jason@Jasons-MacBook-Air.local>
2019-08-11 13:42:46 -0500

    updated additional function using fetch to grab gif information, not currently functional yet

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,72 @@
-const wrestlers = ["hulk hogan","andre the giant","mr perfect","the rock","roudy piper","vince mcmahon","jake the snake roberts","the ultimate warrior","the undertaker","sean michaels" ]
+
+//-------------global variables and render buttons from search function--------//
+
+
+const wrestlers = ["hulkhogan","andrethegiant","mrperfect","therock","roudypiper","vincemcmahon","jakethesnakeroberts","theultimatewarrior","theundertaker","shawnmichaels" ]
 
 
 function renderButtons() {
 
-        
-        document.getElementById("buttons-view").innerHTML = "";
+    document.getElementById("wrestler-buttons").innerHTML = "";
 
-        
         for (let i = 0; i < wrestlers.length; i++) {
 
-         
           const a = document.createElement("button");
           a.classList.add("wrestler");
-          a.setAttribute("data-name", wrestlers[i]);
+          a.setAttribute("data-person", wrestlers[i]);
           a.innerHTML = wrestlers[i];
-          document.getElementById("buttons-view").append(a);
+          document.getElementById("wrestler-buttons").append(a);
 
-        
-        }
-      }
 
-      document.getElementById("add-wrestler").addEventListener("click", function(event) {
+        }  
+    } 
+        renderButtons();
+    document.getElementById("add-wrestler").addEventListener("click", function(event) {
         event.preventDefault();
 
         let wrestler = document.getElementById("wrestler-input").value.trim();
 
         wrestlers.push(wrestler);
-        console.log(wrestlers);
+        
 
         renderButtons();
-      });
+    });
 
-      renderButtons();
+        
+// ---------------------------fetch section ----------------------------- //
+
+function displayWrestlerInfo() {
+    document.querySelectorAll("button").forEach(function(button) {
+        button.addEventListener("Click", function () {
+            
+        const person = this.getAttribute("data-person");    
+        const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=0R2seTXc9SmksHcymZzrTgcyC2E9FL88&q&limit=10&rating=g";
+          
+    fetch(queryURL)
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (responseJson) {
+
+      const wrestlerDiv = document.createElement("div");
+      wrestlerDiv.classList.add("wrestler-div");
+
+      const rating = responseJson.Rated;
+
+      const pOne = document.createElement("p")
+      pOne.innerHTML = "Rating: " + rating;
+
+      wrestlerDiv.append(pOne);
+
+      const imgURL = responseJson.GIF;
+
+      const image = document.createElement("img")
+      image.setAttribute("src", imgURL);
+
+         wrestlerDiv.append(image);
+
+      document.getElementById("wrestlers-view").prepend(wrestlerDiv);
+    });
+});
+});
+};
